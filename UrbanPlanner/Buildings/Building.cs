6e6024e9c3570fd14dcd44b0f9f419ff3292389f
jim-conner <jamesdavidconner@gmail.com>
2021-07-26 09:30:37 -0500

    Add project files.

---
+++ UrbanPlanner/Buildings/Building.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UrbanPlanner.Buildings
+{
+    class Building
+    {
+        // fields (private)
+        string _designer;
+        DateTime _dateConstructed;
+        string _address;
+        string _owner;
+
+        // properties (public)
+        public int Stories { get; set; }
+        public double Width { get; set; }
+        public double Depth { get; set; }
+        public double Volume
+        {
+            get
+            {
+                double calcStories = Convert.ToDouble(Stories);
+                return Width * Depth * (calcStories * 3);
+            }
+        }
+
+        //constructor method for address
+        public Building(int stories, double width, double depth, string address, string designer)
+        {
+            // set field's value to arg value
+            Stories = stories;
+            Width = width;
+            Depth = depth;
+            _designer = designer;
+            _address = address; //I'm setting this field here only and not for class?
+
+            var formattedAddress = address.PadRight(15 + address.Length, '-');
+            Console.WriteLine(
+                $"{formattedAddress}\r\n" +
+                $"Designed By {designer}\r\n" +
+                $"{Volume} cubic meters of space"
+                );
+        }
+
+        public void Construct()
+        {
+            _dateConstructed = DateTime.Now;
+            Console.WriteLine($"Consructed on {_dateConstructed}");
+        }
+
+        public void Purchase(string name)
+        {
+            //name = _owner; this is wrong
+            _owner = name; //this is correct - you're passing in name
+            Console.WriteLine($"Owned by {name}");
+            Console.WriteLine();
+        }
+    }
+}
---
+++ UrbanPlanner/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UrbanPlanner.Buildings;
+
+namespace UrbanPlanner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Building Capitol = new Building(5, 1141, 131, "123 Main Street", "Jim");
+            Capitol.Construct();
+            Capitol.Purchase("We, the People");
+
+            Building Library = new Building(10, 1245, 111, "456 Read Ave", "Jim");
+            Library.Construct();
+            Library.Purchase("Mr. Rogers");
+
+
+            Building Batman = new Building(33, 1832, 666, "333 Commerce Street", "Jim");
+            Batman.Construct();
+            Batman.Purchase("At&t or Bruce Waye");
+            //Console.WriteLine($"{Batman.Volume} cubic meters of space");
+        }
+    }
+}
